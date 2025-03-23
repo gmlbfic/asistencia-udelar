71c7e90603886105cdf3bb1f6b340be3f02f73e0
Gonzalo <gonzalo.lopez@fic.edu.uy>
2025-03-23 18:24:27 -0300

    Fix CSP headers to allow reCAPTCHA eval

--- next.config.js
+++ next.config.js
@@ -1,16 +1,19 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  headers() {
+  async headers() {
     return [
       {
         source: "/(.*)",
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' *.google.com *.gstatic.com"
+            value: "default-src *; script-src 'self' 'unsafe-eval' 'unsafe-inline' *.google.com *.gstatic.com; connect-src *; img-src * data:; style-src 'self' 'unsafe-inline' *"
           }
         ]
       }
     ]
   }
 }
+
+module.exports = nextConfig
